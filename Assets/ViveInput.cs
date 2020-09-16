fb4525a66aebebc7ff8c9fb16a1fd566b7d0ebfb
Wes (Predator 900) <whawkins@virbela.com>
2020-09-16 13:43:37 -0700

    Seed

---
+++ Assets/ViveInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+public class ViveInput : MonoBehaviour
+{
+    public SteamVR_Action_Single squeezeAction = SteamVR_Input.GetAction<SteamVR_Action_Single>("Squeeze", true);
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Input.GetStateDown("default", "Teleport", SteamVR_Input_Sources.Any, true))
+        {
+            Debug.Log("Teleport button pressed");
+        }
+        if (SteamVR_Input.GetStateUp("default", "GrabPinch", SteamVR_Input_Sources.Any, true))
+        {
+            Debug.Log("Pinch button pressed");
+        }
+        float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.Any);
+        if(triggerValue > 0.0f)
+        {
+            Debug.Log(triggerValue);
+        }
+    }
+}
